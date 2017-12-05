df080852ae528adcb87d2976244adf2a623711ed
MariaViolaki <mariaviolaki@outlook.com>
2017-12-05 14:27:23 +0200

    Display human score as percentage

--- app/src/main/java/com/example/android/humanorcharacterquiz/MainActivity.java
+++ app/src/main/java/com/example/android/humanorcharacterquiz/MainActivity.java
@@ -7,6 +7,7 @@ import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,93 +26,93 @@ public class MainActivity extends AppCompatActivity {
         int personality_points = 0;
 
         RadioButton oneOne = findViewById(R.id.one_1);
-        RadioButton oneTwo = findViewById(R.id.one_2);
+        RadioButton oneThree = findViewById(R.id.one_3);
 
         if (oneOne.isChecked()) {
+            personality_points += +10;
+        } else if (oneThree.isChecked()) {
             personality_points += +5;
-        } else if (oneTwo.isChecked()) {
-            personality_points += -5;
         }
 
         RadioButton twoThree = findViewById(R.id.two_3);
-        RadioButton twoOne = findViewById(R.id.two_1);
+        RadioButton twoTwo = findViewById(R.id.two_2);
 
         if (twoThree.isChecked()) {
+            personality_points += +10;
+        } else if (twoTwo.isChecked()) {
             personality_points += +5;
-        } else if (twoOne.isChecked()) {
-            personality_points += -5;
         }
 
         RadioButton threeOne = findViewById(R.id.three_1);
-        RadioButton threeTwo = findViewById(R.id.three_2);
+        RadioButton threeThree = findViewById(R.id.three_3);
 
         if (threeOne.isChecked()) {
+            personality_points += +10;
+        } else if (threeThree.isChecked()) {
             personality_points += +5;
-        } else if (threeTwo.isChecked()) {
-            personality_points += -5;
         }
 
         RadioButton fourTwo = findViewById(R.id.four_2);
-        RadioButton fourThree = findViewById(R.id.four_3);
+        RadioButton fourOne = findViewById(R.id.four_1);
 
         if (fourTwo.isChecked()) {
+            personality_points += +10;
+        } else if (fourOne.isChecked()) {
             personality_points += +5;
-        } else if (fourThree.isChecked()) {
-            personality_points += -5;
         }
 
         RadioButton fiveTwo = findViewById(R.id.five_2);
-        RadioButton fiveOne = findViewById(R.id.five_1);
+        RadioButton fiveThree = findViewById(R.id.five_3);
 
         if (fiveTwo.isChecked()) {
+            personality_points += +10;
+        } else if (fiveThree.isChecked()) {
             personality_points += +5;
-        } else if (fiveOne.isChecked()) {
-            personality_points += -5;
         }
 
         RadioButton sixOne = findViewById(R.id.six_1);
-        RadioButton sixTwo = findViewById(R.id.six_2);
+        RadioButton sixThree = findViewById(R.id.six_3);
 
         if (sixOne.isChecked()) {
+            personality_points += +10;
+        } else if (sixThree.isChecked()) {
             personality_points += +5;
-        } else if (sixTwo.isChecked()) {
-            personality_points += -5;
         }
 
         RadioButton sevenThree = findViewById(R.id.seven_3);
-        RadioButton sevenTwo = findViewById(R.id.seven_2);
+        RadioButton sevenOne = findViewById(R.id.seven_1);
 
         if (sevenThree.isChecked()) {
+            personality_points += +10;
+        } else if (sevenOne.isChecked()) {
             personality_points += +5;
-        } else if (sevenTwo.isChecked()) {
-            personality_points += -5;
         }
 
         RadioButton eightTwo = findViewById(R.id.eight_2);
-        RadioButton eightOne = findViewById(R.id.eight_1);
+        RadioButton eightThree = findViewById(R.id.eight_3);
 
         if (eightTwo.isChecked()) {
+            personality_points += +10;
+        } else if (eightThree.isChecked()) {
             personality_points += +5;
-        } else if (eightOne.isChecked()) {
-            personality_points += -5;
         }
 
         RadioButton nineTwo = findViewById(R.id.nine_2);
-        RadioButton nineThree = findViewById(R.id.nine_3);
+        RadioButton nineOne = findViewById(R.id.nine_1);
 
         if (nineTwo.isChecked()) {
+            personality_points += +10;
+        } else if (nineOne.isChecked()) {
             personality_points += +5;
-        } else if (nineThree.isChecked()) {
-            personality_points += -5;
         }
 
         RadioButton tenOne = findViewById(R.id.ten_1);
-        RadioButton tenThree = findViewById(R.id.ten_3);
+        RadioButton tenTwo = findViewById(R.id.ten_2);
 
         if (tenOne.isChecked()) {
+            personality_points += +10;
+        } else if (tenTwo.isChecked()) {
             personality_points += +5;
-        } else if (tenThree.isChecked()) {
-            personality_points += -5;
         }
 
         return personality_points;
@@ -129,22 +130,22 @@ public class MainActivity extends AppCompatActivity {
 
         ImageView imageResult = findViewById(R.id.image_result);
 
-        if (calculateResults() >= 35) {
+        if (calculateResults() >= 80) {
             resultsTitle = getString(R.string.very_human_title);
             resultsBody = getString(R.string.very_human);
             imageResult.setImageResource(R.drawable.very_human);
         }
-        else if (calculateResults() >= 15) {
+        else if (calculateResults() >= 60) {
             resultsTitle = getString(R.string.human_title);
             resultsBody = getString(R.string.human);
             imageResult.setImageResource(R.drawable.human);
         }
-        else if (calculateResults() >= -15) {
+        else if (calculateResults() >= 40) {
             resultsTitle = getString(R.string.unsure_title);
             resultsBody = getString(R.string.unsure);
             imageResult.setImageResource(R.drawable.unsure);
         }
-        else if (calculateResults() >= -35) {
+        else if (calculateResults() >= 20) {
             resultsTitle = getString(R.string.character_title);
             resultsBody = getString(R.string.character);
             imageResult.setImageResource(R.drawable.character);
@@ -160,7 +161,7 @@ public class MainActivity extends AppCompatActivity {
         TextView quizResults = findViewById(R.id.quiz_results);
         quizResultsTitle.setPadding(0,20,0,20);
         humanScore.setPadding(0,100,0,0);
-        humanScore.setText(getString(R.string.score) + " " + calculateResults());
+        humanScore.setText(getString(R.string.score, calculateResults()));
         quizResultsTitle.setText(resultsTitle);
         quizResults.setText(resultsBody);
 
