b703fff0562d51e8f85ea725d05d9f59aee419ed
MariaViolaki <mariaviolaki@outlook.com>
2018-01-22 01:44:59 +0200

    Added email intent, checkbox, edittext

--- app/src/main/java/com/example/android/humanorcharacterquiz/MainActivity.java
+++ app/src/main/java/com/example/android/humanorcharacterquiz/MainActivity.java
@@ -1,9 +1,15 @@
 package com.example.android.humanorcharacterquiz;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
@@ -11,17 +17,18 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    String resultsTitle;
+    String resultsBody;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
-
-//    This method calculates the final score.
-//    Each RadioButton can offer +5, -5 or 0 points to the final score.
-
-    public int calculateResults() {
+    //    This method calculates the final score.
+    //    Each RadioButton can offer +5, -5 or 0 points to the final score.
+    private int calculateAnswers() {
 
         int personality_points = 0;
 
@@ -118,56 +125,117 @@ public class MainActivity extends AppCompatActivity {
         return personality_points;
     }
 
+    private String findResultsTitle() {
+        EditText nameGiven = findViewById(R.id.name_given);
+        String name = nameGiven.getText().toString();
 
+        if (calculateAnswers() >= 80) {
+            resultsTitle = getString(R.string.very_human_title, name);
+        } else if (calculateAnswers() >= 60) {
+            resultsTitle = getString(R.string.human_title, name);
+        } else if (calculateAnswers() >= 40) {
+            resultsTitle = getString(R.string.unsure_title, name);
+        } else if (calculateAnswers() >= 20) {
+            resultsTitle = getString(R.string.character_title, name);
+        } else {
+            resultsTitle = getString(R.string.very_character_title, name);
+        }
+        return resultsTitle;
+    }
 
-//    This method shows the results of the points calculated in the previous method.
-//    The image, score and text vary (5 categories in total).
-
+    private String findResultsBody() {
+        if (calculateAnswers() >= 80) {
+            resultsBody = getString(R.string.very_human);
+        } else if (calculateAnswers() >= 60) {
+            resultsBody = getString(R.string.human);
+        } else if (calculateAnswers() >= 40) {
+            resultsBody = getString(R.string.unsure);
+        } else if (calculateAnswers() >= 20) {
+            resultsBody = getString(R.string.character);
+        } else {
+            resultsBody = getString(R.string.very_character);
+        }
+        return resultsBody;
+    }
+    
+    //    This method shows the results of the points calculated in the previous method.
+    //    The image, score and text vary (5 categories in total).
     public void showResults(View view) {
 
-        String resultsTitle;
-        String resultsBody;
+        EditText nameGiven = findViewById(R.id.name_given);
+        RadioGroup radioGroup1 = findViewById(R.id.radio_group_1);
+        RadioGroup radioGroup2 = findViewById(R.id.radio_group_2);
+        RadioGroup radioGroup3 = findViewById(R.id.radio_group_3);
+        RadioGroup radioGroup4 = findViewById(R.id.radio_group_4);
+        RadioGroup radioGroup5 = findViewById(R.id.radio_group_5);
+        RadioGroup radioGroup6 = findViewById(R.id.radio_group_6);
+        RadioGroup radioGroup7 = findViewById(R.id.radio_group_7);
+        RadioGroup radioGroup8 = findViewById(R.id.radio_group_8);
+        RadioGroup radioGroup9 = findViewById(R.id.radio_group_9);
+        RadioGroup radioGroup10 = findViewById(R.id.radio_group_10);
 
-        ImageView imageResult = findViewById(R.id.image_result);
+        // Check if all answers are answered and if a name is entered
+        if ((radioGroup1.getCheckedRadioButtonId() != -1) && (radioGroup2.getCheckedRadioButtonId() != -1) &&
+                (radioGroup3.getCheckedRadioButtonId() != -1) && (radioGroup4.getCheckedRadioButtonId() != -1) &&
+                (radioGroup5.getCheckedRadioButtonId() != -1) && (radioGroup6.getCheckedRadioButtonId() != -1) &&
+                (radioGroup7.getCheckedRadioButtonId() != -1) && (radioGroup8.getCheckedRadioButtonId() != -1) &&
+                (radioGroup9.getCheckedRadioButtonId() != -1) && (radioGroup10.getCheckedRadioButtonId() != -1) &&
+                (nameGiven.getText().toString().trim().length() > 0)) {
 
-        if (calculateResults() >= 80) {
-            resultsTitle = getString(R.string.very_human_title);
-            resultsBody = getString(R.string.very_human);
-            imageResult.setImageResource(R.drawable.very_human);
-        }
-        else if (calculateResults() >= 60) {
-            resultsTitle = getString(R.string.human_title);
-            resultsBody = getString(R.string.human);
-            imageResult.setImageResource(R.drawable.human);
-        }
-        else if (calculateResults() >= 40) {
-            resultsTitle = getString(R.string.unsure_title);
-            resultsBody = getString(R.string.unsure);
-            imageResult.setImageResource(R.drawable.unsure);
-        }
-        else if (calculateResults() >= 20) {
-            resultsTitle = getString(R.string.character_title);
-            resultsBody = getString(R.string.character);
-            imageResult.setImageResource(R.drawable.character);
-        }
-        else  {
-            resultsTitle = getString(R.string.very_character_title);
-            resultsBody = getString(R.string.very_character);
-            imageResult.setImageResource(R.drawable.very_character);
-        }
+            // Make results visible
+            LinearLayout results = findViewById(R.id.results);
+            results.setVisibility(View.VISIBLE);
+            nameGiven.clearFocus();
+            results.requestFocus();
 
-        TextView humanScore = findViewById(R.id.human_score);
-        TextView quizResultsTitle = findViewById(R.id.quiz_results_title);
-        TextView quizResults = findViewById(R.id.quiz_results);
-        quizResultsTitle.setPadding(0,20,0,20);
-        humanScore.setPadding(0,100,0,0);
-        humanScore.setText(getString(R.string.score, calculateResults()));
-        quizResultsTitle.setText(resultsTitle);
-        quizResults.setText(resultsBody);
+            // Show the correct image
+            ImageView imageResult = findViewById(R.id.image_result);
+            if (calculateAnswers() >= 80) {
+                imageResult.setImageResource(R.drawable.very_human);
+            } else if (calculateAnswers() >= 60) {
+                imageResult.setImageResource(R.drawable.human);
+            } else if (calculateAnswers() >= 40) {
+                imageResult.setImageResource(R.drawable.unsure);
+            } else if (calculateAnswers() >= 20) {
+                imageResult.setImageResource(R.drawable.character);
+            } else {
+                imageResult.setImageResource(R.drawable.very_character);
+            }
+
+            // Show results on screen
+            TextView humanScore = findViewById(R.id.human_score);
+            TextView quizResultsTitle = findViewById(R.id.quiz_results_title);
+            TextView quizResults = findViewById(R.id.quiz_results);
+            humanScore.setText(getString(R.string.score, calculateAnswers()));
+            quizResultsTitle.setText(findResultsTitle());
+            quizResults.setText(findResultsBody());
+
+            // Show a toast in case of unanswered questions and empty name box
+        } else if (nameGiven.getText().toString().isEmpty()) {
+            Toast.makeText(this, getString(R.string.name_toast), Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, getString(R.string.radiobuttons_toast), Toast.LENGTH_SHORT).show();
+        }
 
     }
 
+    public void quit(View view) {
+
+        // Check if the user wants to share the results before quitting
+        CheckBox email = findViewById(R.id.email);
+        if (email.isChecked()) {
+            Intent intent = new Intent(Intent.ACTION_SENDTO);
+            intent.setData(Uri.parse("mailto:"));
+            intent.putExtra(Intent.EXTRA_SUBJECT, findResultsTitle());
+            intent.putExtra(Intent.EXTRA_TEXT, findResultsBody());
+            if (intent.resolveActivity(getPackageManager()) != null) {
+                startActivity(intent);
+            }
+        }
+        finish();
     }
+
+}
 
 
 
